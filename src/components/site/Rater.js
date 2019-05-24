06e229ef21a265cb2f199eb63c9eb78730527e53
Marco Monter <monter.marco@gmail.com>
2019-05-24 10:14:24 -0500

    Borrado Location.js

--- src/components/site/Rater.js
+++ src/components/site/Rater.js
@@ -5,21 +5,21 @@ const desc = ['terrible', 'malo', 'normal', 'bueno', 'excelente'];
 
 class Rater extends React.Component {
   state = {
-    rank: 3,
+    value: 3,
   };
 
-  handleChange = rank => {
-    this.setState({ rank });
-    const rated = rank;
+  handleChange = value => {
+    this.setState({ value });
+    const rated = value;
     console.log(rated)
   };
 
   render() {
-    const { rank } = this.state;
+    const { value } = this.state;
     return (
       <span>
-        <Rate onChange={this.handleChange} rank={rank} />
-        {rank ? <span className="ant-rate-text">{desc[rank - 1]}</span> : ''}
+        <Rate onChange={this.handleChange} value={value} />
+        {value ? <span className="ant-rate-text">{desc[value - 1]}</span> : ''}
       </span>
     );
   }
